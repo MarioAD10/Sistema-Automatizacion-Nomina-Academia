e591822e9278316d99b08b3ccebf96ea96393097
Mario <duranmario480@gmail.com>
2026-03-04 11:54:55 -0400

    SNA-7 / SNA-8 / SNA-9 Realizados

---
+++ SistemaAutomatizacionNomina.Entities/Entities/Login/E_Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAutomatizacionNomina.Entities.Entities.Login
+{
+    public class E_Usuarios
+    {
+        private int idUsuario;
+        private string nombre;
+        private string password;
+        private string rol;
+
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Password { get => password; set => password = value; }
+        public string Rol { get => rol; set => rol = value; }
+    }
+}
