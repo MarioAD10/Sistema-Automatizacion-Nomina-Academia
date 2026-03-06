6690d84ac1c96c54b0d3eb6a8724eb717afd7ab1
Mario <duranmario480@gmail.com>
2026-03-05 22:18:08 -0400

    Backend de login

---
+++ SistemaAutomatizacionNomina.DAL/Repositories/Login/D_Usuarios.cs
@@ -0,0 +1,41 @@
+﻿using SistemaAutomatizacionNomina.Entities.Entities.Login;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAutomatizacionNomina.DAL.Repositories.Login
+{
+    public class D_Usuarios
+    {
+        string conexion = ConfigurationManager.ConnectionStrings["Conectar"].ConnectionString;
+
+        public E_Usuarios Login(string nombre, string password)
+        {
+            using (SqlConnection cnx = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_LoginUsuario", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", nombre);
+                cmd.Parameters.AddWithValue("@Password", password);
+
+                cnx.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    return new E_Usuarios
+                    {
+                        Nombre = nombre,
+                        Password = password,
+                        Rol = dr["Rol"].ToString()
+                    };
+                }
+                return null;
+            }
+        }
+    }
+}
